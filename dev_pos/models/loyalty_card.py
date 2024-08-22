a10a743ab88a5bab4049e40846438898d85712e5
abhiramatech <131126326+abhiramatech@users.noreply.github.com>
2024-08-22 14:54:46 +0700

    ADD Code Store 02

---
+++ dev_pos/models/loyalty_card.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models
+
+
+class LoyaltyCardInherit(models.Model):
+    _inherit = 'loyalty.card'
+
+    is_integrated = fields.Boolean(string="Integrated", default=False, readonly=True, tracking=True)
+    is_updated = fields.Boolean(string="Updated", default=False, readonly=True, tracking=True)  
+
+    def write(self, vals):
+        # Only set is_updated to True if it hasn't been explicitly set to False in vals
+        if 'is_updated' not in vals:
+            vals['is_updated'] = True
+
+        return super(LoyaltyCardInherit, self).write(vals)
---
+++ dev_pos/models/pos_payment_method.py
@@ -0,0 +1,11 @@
+import requests
+from datetime import datetime, timedelta
+import pytz
+from odoo import models, fields, api, SUPERUSER_ID
+from odoo.exceptions import UserError
+
+class PoSPaymentMethodInherit(models.Model):
+    _inherit = 'pos.payment.method'
+
+    is_integrated = fields.Boolean(string="Integrated", default=False)
+    is_updated = fields.Boolean(string="Updated", default=False)
